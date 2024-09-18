7de88affca155175b9769f8b0beafb76077b5dae
Anish Kumar <kanish92729@gmail.com>
2024-09-18 10:55:51 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const { Server } = require("socket.io");
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
-    cors: "https://ttt-multiplayer-ak.vercel.app/",
+    cors: "https://ttt-multiplayer-ak.vercel.app",
 });
 
 const allUsers = {};
